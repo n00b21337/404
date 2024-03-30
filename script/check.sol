8acc924efa7a05e04c43889580a39759b0901ed7
Mark.B <adriadrop@gmail.com>
2024-03-31 00:13:20 +0100

    add file reading and work on trying to import contract address

--- script/check.sol
+++ script/check.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
+import "forge-std/StdJson.sol";
 
 interface ITH404 {
     function owner() external returns (address);
@@ -13,15 +14,24 @@ interface ITH404 {
 }
 
 contract CheckScript is Script {
-    function run() external {
+    function run() external returns (Tx1559 memory) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         ITH404 th = ITH404(0xc5D38Bcc8E0B8a7C61bef5b1cc8945118a0Fd712);
         string memory symbol = th.symbol();
         console.logString(symbol);
 
-        console.log(th.owner());
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(
+            root,
+            "/broadcast/deploy.sol/666666666/run-latest.json"
+        );
+
+        // Tx1559[] memory transactions = readTx1559s(path);
+
+        // console.log(transactions[0].contractAddress);
         vm.stopBroadcast();
-        // return th.owner();
+
+        //return transactions[0];
     }
 }
