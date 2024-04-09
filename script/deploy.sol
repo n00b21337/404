f7ffe1a79e12d9174b494ad1a038d6d803d61668
Marko <adriadrop@gmail.com>
2024-04-09 16:10:38 +0200

    add name

--- script/deploy.sol
+++ script/deploy.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "../src/OG404.sol";
+import "../src/Glyphs404.sol";
 
 contract DeployContract is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        new OG404(msg.sender);
+        new Glyphs404(msg.sender);
 
         vm.stopBroadcast();
     }
